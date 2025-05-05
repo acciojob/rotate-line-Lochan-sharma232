83d9455647a1093276d459b2ef0ce4861fd0bc30
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-05 22:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const rotate=document.getElementById("line");
-style.transform=rotate(deg);
+rotate.style.transform(deg);
 rotate.addEventListener("click",(e)=>{
 	setInterval(function(){rotate.innerHTML+=line},1000);
 
